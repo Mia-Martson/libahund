a64b48afe778b6487b9c4a32aa95aa89f0328865
66nesryytel <siimmattiasmae@gmail.com>
2025-01-25 22:32:02 +0200

    collision siiani ei tööta

--- GameJam2025/Assets/_Scripts/boss/bosshealth.cs
+++ GameJam2025/Assets/_Scripts/boss/bosshealth.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class bosshealth : MonoBehaviour
 {
+    public float maxHealth = 50f;
+    public float currentHealth;
+
+    public GameObject deathEffect; // Optional particle effect or animation for death
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void takeDamage()
+    {
+     //   currentHealth -= damage;
     }
 }

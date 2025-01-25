f3ddccea6ee374c7ec52ebad077dfa1fd751f401
66nesryytel <siimmattiasmae@gmail.com>
2025-01-25 15:32:34 +0200

    bossfightedenebhästi

---
+++ GameJam2025/Assets/boss.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss : MonoBehaviour
+{
+    public GameObject bulletPrefab; // Assign your bullet prefab here
+    public float shootInterval = 1f; // Time between each circle
+    public int numberOfBullets = 20; // How many bullets in the circle
+    public float bulletSpeed = 5f; // Speed of the bullets
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   InvokeRepeating(nameof(Attack), 0f, shootInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+        //ründa mängijat
+        
+            float angleStep = 360f / numberOfBullets; // Angle between each bullet
+            float angle = 0f;
+
+            for (int i = 0; i < numberOfBullets; i++)
+            {
+                // Calculate the direction of the bullet
+                float bulletDirX = Mathf.Cos(angle * Mathf.Deg2Rad);
+                float bulletDirY = Mathf.Sin(angle * Mathf.Deg2Rad);
+                Vector2 bulletDirection = new Vector2(bulletDirX, bulletDirY);
+
+                // Spawn the bullet
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                Bullet bulletScript = bullet.GetComponent<Bullet>();
+                bulletScript.SetDirection(bulletDirection);
+                bulletScript.speed = bulletSpeed;
+
+                // Increment the angle for the next bullet
+                angle += angleStep;
+            
+        }
+    }
+}
